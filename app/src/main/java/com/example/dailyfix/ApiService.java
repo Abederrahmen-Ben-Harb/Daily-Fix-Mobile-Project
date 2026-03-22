44edd7fd2842ffb4ca8f1ea0dddbf921b9b8b871
dali2000 <medalikebair@gmail.com>
2026-03-22 22:07:49 +0100

    fix(register): fix register and login api call

--- app/src/main/java/com/example/dailyfix/ApiService.java
+++ app/src/main/java/com/example/dailyfix/ApiService.java
@@ -5,11 +5,11 @@ import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface ApiService {
-    // Route pour la connexion (POST /api/login)
-    @POST("/auth/login")
+    // Route pour la connexion (POST /api/auth/login)
+    @POST("auth/login")
     Call<LoginResponse> loginUser(@Body User user);
 
-    // Route pour l'inscription (POST /api/register)
-    @POST("/auth/register")
+    // Route pour l'inscription (POST /api/auth/register)
+    @POST("auth/register")
     Call<RegisterResponse> registerUser(@Body User user);
 }
